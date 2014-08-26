95d7b978dc739ff01ce1e5ff563fdd455e67d4f2
MIguel Mayori <mmayorivera@gmail.com>
2014-08-26 12:49:01 -0400

    Home Page Partials

---
+++ app/views/master/banner.blade.php
@@ -0,0 +1,248 @@
+<div id="banner">
+    <div class="tp-banner-holder">
+      <div class="tp-banner-container">
+        <div class="tp-banner">
+          <ul>
+            <li data-transition="fade" data-slotamount="7" data-masterspeed="1000" data-delay="6000" > 
+              <!-- MAIN IMAGE --> 
+              <img src="images/bg_slider_1.jpg" alt="slidebg1" data-bgfit="cover" data-bgposition="center top" data-bgrepeat="no-repeat"> 
+              
+              <!-- LAYER NR. 1 -->
+              <div class="tp-caption large_bold_white customin customout tp-resizeme"
+                  data-x="center" data-hoffset="0"
+                  data-y="center" data-voffset="-200"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="500"
+                  data-easing="Power3.easeInOut"
+                  data-splitin="chars"
+                  data-splitout="chars"
+                  data-elementdelay="0.08"
+                  data-endelementdelay="0.08"
+                  data-endspeed="500"
+                  style="z-index: 2; max-width: auto; max-height: auto; white-space: nowrap;">Education “ everywhere ” </div>
+              
+              <!-- LAYER NR. 2 -->
+              <div class="tp-caption customin customout"
+                  data-x="center" data-hoffset="0"
+                  data-y="center" data-voffset="-140"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1000"
+                  data-easing="Back.easeOut"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.1"
+                  data-endelementdelay="0.1"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap; color:#fff; font-size: 18px;  ">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has </p>
+              </div>
+              
+              <!-- LAYER NR. 3 -->
+              <div class="tp-caption customin customout"
+                  data-x="center" data-hoffset="0"
+                  data-y="center" data-voffset="-100"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1200"
+                  data-easing="Back.easeOut"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.1"
+                  data-endelementdelay="0.1"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap; color:#fff; font-size: 18px;  ">
+                <p>been the industry's standard dummy text ever since the 1500s, when an.</p>
+              </div>
+              
+              <!-- LAYER NR. 4 -->
+              <div class="tp-caption customin customout"
+                  data-x="center" data-hoffset="-120"
+                  data-y="center" data-voffset="-30"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1400"
+                  data-easing="Back.easeOut"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.1"
+                  data-endelementdelay="0.1"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  style="z-index: 4; "> <a href="#." class="button btn res">DOWNLOAD NOW!</a> </div>
+              
+              <!-- LAYER NR. 5 -->
+              <div class="tp-caption customin customout"
+                  data-x="center" data-hoffset="120"
+                  data-y="center" data-voffset="-30"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1400"
+                  data-easing="Back.easeOut"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.1"
+                  data-endelementdelay="0.1"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  style="z-index: 4; "> <a href="#." class="button btn btn_blue">LEARN MORE</a> </div>
+              
+              <!-- LAYER NR. 6 -->
+              <div class="tp-caption lfb skewtoleft customout"
+                  data-x="center" data-hoffset="-300"
+                  data-y="bottom" data-voffset="-80"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1700"
+                  data-easing="Back.easeOut"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  data-captionhidden="on"
+                  style="z-index: 1;"><img src="images/iphone.png" alt=""> </div>
+              
+              <!-- LAYER NR. 7 -->
+              <div class="tp-caption lfb skewtoleft customout"
+                  data-x="center" data-hoffset="-100"
+                  data-y="bottom" data-voffset="-80"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1900"
+                  data-easing="Back.easeOut"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  data-captionhidden="on"
+                  style="z-index: 1;"><img src="images/ipad.png" alt=""> </div>
+              
+              <!-- LAYER NR. 8 -->
+              <div class="tp-caption lfb skewtoleft customout"
+                  data-x="center" data-hoffset="200"
+                  data-y="bottom" data-voffset="-90"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="2100"
+                  data-easing="Back.easeOut"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  data-captionhidden="on"
+                  style="z-index: 1;"><img src="images/mac.png" alt=""> </div>
+            </li>
+            
+            <!-- Slide NR. 1 -->
+            <li data-transition="random" data-slotamount="7" data-masterspeed="1000" data-delay="6000" > 
+              <!-- MAIN IMAGE --> 
+              <img src="images/bg_slider_2.jpg" alt="slidebg1" data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat"> 
+              
+              <!-- LAYER NR. 1 -->
+              <div class="tp-caption large_bold_white customin customout tp-resizeme"
+                  data-x="center" data-hoffset="0"
+                  data-y="center" data-voffset="-200"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="500"
+                  data-easing="Power3.easeInOut"
+                  data-splitin="chars"
+                  data-splitout="chars"
+                  data-elementdelay="0.08"
+                  data-endelementdelay="0.08"
+                  data-endspeed="500"
+                  style="z-index: 2; max-width: auto; max-height: auto; white-space: nowrap;">Perfect Learning Systems </div>
+              
+              <!-- LAYER NR. 2 -->
+              <div class="tp-caption customin customout"
+                  data-x="center" data-hoffset="0"
+                  data-y="center" data-voffset="-140"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1000"
+                  data-easing="Back.easeOut"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.1"
+                  data-endelementdelay="0.1"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap; color:#fff; font-size: 18px;  ">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has </p>
+              </div>
+              
+              <!-- LAYER NR. 2 -->
+              <div class="tp-caption customin customout"
+                  data-x="center" data-hoffset="0"
+                  data-y="center" data-voffset="-100"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1200"
+                  data-easing="Back.easeOut"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.1"
+                  data-endelementdelay="0.1"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  style="z-index: 4; max-width: auto; max-height: auto; white-space: nowrap; color:#fff; font-size: 18px;  ">
+                <p>been the industry's standard dummy text ever since the 1500s, when an.</p>
+              </div>
+              
+              <!-- LAYER NR. 3 -->
+              <div class="tp-caption customin customout"
+                  data-x="center" data-hoffset="120"
+                  data-y="center" data-voffset="200"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1400"
+                  data-easing="Back.easeOut"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.1"
+                  data-endelementdelay="0.1"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  style="z-index: 4; "> <a href="#." class="button btn res">DOWNLOAD NOW!</a> </div>
+              
+              <!-- LAYER NR. 4 -->
+              <div class="tp-caption customin customout"
+                  data-x="center" data-hoffset="-120"
+                  data-y="center" data-voffset="200"
+                  data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0;scaleY:0;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="1400"
+                  data-easing="Back.easeOut"
+                  data-splitin="none"
+                  data-splitout="none"
+                  data-elementdelay="0.1"
+                  data-endelementdelay="0.1"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  style="z-index: 4; "> <a href="#." class="button btn btn_blue">LEARN MORE</a> </div>
+              
+              <!-- LAYER NR. 5 -->
+              <div class="tp-caption lfb skewtoleft customout"
+                  data-x="center" data-hoffset="0"
+                  data-y="center" data-voffset="250"
+                  data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                  data-speed="500"
+                  data-start="700"
+                  data-easing="Back.easeOut"
+                  data-endspeed="500"
+                  data-endeasing="Power4.easeIn"
+                  data-captionhidden="on"
+                  style="z-index: 1;"><img class="img-responsive" src="images/banner_brows.png" alt=""> </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
