db1304358813adf29659b77cf1a6223e85e8f712
MIguel Mayori <mmayorivera@gmail.com>
2014-08-26 12:42:01 -0400

    Home Page Partials

--- app/views/master/header.blade.php
+++ app/views/master/header.blade.php
@@ -1,8 +1,35 @@
-
-<?php
-/**
- * Created by PhpStorm.
- * User: carolasilvateran
- * Date: 8/25/14
- * Time: 9:58 PM
- */ 
+<header>
+    <div class="container">
+      <div class="logo"> <a href="#wrap"> <img src="images/logo.png" alt=""> </a> </div>
+      <ul class="social_icons">
+        <li  class="facebook"> <a href="#."><i class="ion-social-facebook"></i> </a> </li>
+        <li class="twitter"> <a href="#."><i class="ion-social-twitter"></i> </a> </li>
+        <li class="dribbble"> <a href="#."><i class="ion-social-dribbble"></i> </a> </li>
+        <li class="googleplus"> <a href="#."><i class="ion-social-googleplus"></i> </a> </li>
+        <li class="linkedin"> <a href="#."><i class="ion-social-linkedin"></i> </a> </li>
+      </ul>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="ion-navicon-round"></span> </button>
+      </div>
+      <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#banner">HOME</a></li>
+                <li><a href="#det_fea">Features</a></li>
+                <li><a href="#portfolio">Classes </a></li>
+                <li><a href="#achievements">Countdown </a></li>
+                <li class="miss"><a href="#price_table">Pricing </a></li>
+                <li><a href="#testimonial">Testimonials </a></li>
+                <li><a href="#footer">Contact </a></li>
+              </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+          </div>
+          <!-- /.container-fluid -->
+        </div>
+      </nav>
+    </div>
+</header>
