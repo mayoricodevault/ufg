3276cc381a51156f639942d64862d57862d4665a
MIguel Mayori <mmayorivera@gmail.com>
2014-09-22 11:44:52 -0400

    Home Page Partials

--- app/views/master/footer.blade.php
+++ app/views/master/footer.blade.php
@@ -1,18 +1,15 @@
 <section id="footer" class="bg_color_4">
-      <h2 class="wow tada" data-wow-delay="200ms">Get in touch</h2>
-      <hr>
-        <ul class="social_icons">
-          <li class="facebook wow fadeInUpBig" data-wow-delay="100ms"> <a href="http://www.facebook.com/ufguidance"><i class="ion-social-facebook"></i> </a> </li>
-        </ul>
-        <div class="links">
-        <nav>
-          <ul>
-            <li>© 2014 Copyright - Redefined Ind.</li>
-          </ul>
-         </nav>
-        </div>
-      </div>
-    </section>
+    <ul class="social_icons">
+      <li class="facebook wow fadeInUpBig" data-wow-delay="100ms"> <a href="http://www.facebook.com/ufguidance"><i class="ion-social-facebook"></i> </a> </li>
+    </ul>
+    <div class="links">
+    <nav>
+      <ul>
+        <li>© 2014 Copyright - Redefined Ind.</li>
+      </ul>
+     </nav>
+    </div>
+</section>
     <div class="col-lg-12 text-center">
       <div class="back-to-top"> <i class="ion-ios7-arrow-up"></i> </div>
     </div>
