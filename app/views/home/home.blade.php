fac8dd8b40f86c97a9d476127bf39948bd38f415
MIguel Mayori <mmayorivera@gmail.com>
2014-09-22 12:16:16 -0400

    Home Page Partials

--- app/views/home/home.blade.php
+++ app/views/home/home.blade.php
@@ -100,3 +100,15 @@
     @include('master/footer')
   </div>
 @stop
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'ufguidance'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+</script>
