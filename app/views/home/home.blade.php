cd6c7eb0a607504afc01804e9fd8d64d8d555206
MIguel Mayori <mmayorivera@gmail.com>
2014-09-22 12:15:50 -0400

    Home Page Partials

--- app/views/home/home.blade.php
+++ app/views/home/home.blade.php
@@ -81,10 +81,20 @@
 
     <div id="phone_number">
       <div class="container">
-        <div class="number wow rollIn" data-wow-delay="200ms"> <i class="ion-iphone"></i> <span> 123-45676-789</span></div>
-        <div class="address">
-          <p>Call from Monday to Friday from <strong>12:00</strong>to <strong>14:00</strong> and from <strong> 14:00 </strong> to <strong>22:00</strong></p>
-        </div>
+          <div id="disqus_thread"></div>
+          <script type="text/javascript">
+              /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+              var disqus_shortname = 'ufguidance'; // required: replace example with your forum shortname
+
+              /* * * DON'T EDIT BELOW THIS LINE * * */
+              (function() {
+                  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+              })();
+          </script>
+          <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+          <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
       </div>
     </div>
     @include('master/footer')
